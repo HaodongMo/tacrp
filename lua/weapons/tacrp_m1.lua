d2d7b280bec1cdf4c54b4d81359765d925536567
TheOnly8Z <888888zombies@gmail.com>
2022-07-05 20:21:08 -0500

    add mini-14 and mini uzi

---
+++ lua/weapons/tacrp_m1.lua
@@ -0,0 +1,197 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "Ruger Mini-14"
+SWEP.Category = "Tactical RP (Arctic)"
+
+SWEP.Description = "Cheap semi-automatic rifle with no stock and no optic mount."
+
+SWEP.ViewModel = "models/weapons/tacint/v_m1.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_m1.mdl"
+
+SWEP.Slot = 3
+
+// "ballistics"
+
+SWEP.Damage_Max = 100 / 4 // damage at minimum range
+SWEP.Damage_Min = 100 / 6 // damage at maximum range
+SWEP.Range_Min = 2000 // distance for which to maintain maximum damage
+SWEP.Range_Max = 6000 // distance at which we drop to minimum damage
+SWEP.Penetration = 7 // units of metal this weapon can penetrate
+
+// misc. shooting
+
+SWEP.Firemode = 1
+
+SWEP.RPM = 450
+
+SWEP.Spread = 0.0035
+
+// Spread penalties are in spread units and are additive
+SWEP.MoveSpreadPenalty = 0.0 // spread penalty while travelling at max. 250 u/s
+SWEP.MidAirSpreadPenalty = 0.0 // spread penalty for being in the air
+SWEP.HipFireSpreadPenalty = 0.0 // spread penalty for not being scoped in
+SWEP.ScopedSpreadPenalty = 0
+SWEP.BlindFireSpreadPenalty = 0.0 // spread penalty for blind firing
+SWEP.CrouchSpreadPenalty = 0
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 5
+SWEP.RecoilResetTime = 0.25 // time after you stop shooting for recoil to start dissipating
+SWEP.RecoilDissipationRate = 100
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 1
+
+SWEP.RecoilKick = 4
+
+
+SWEP.RecoilSpreadPenalty = 0.0025 // extra spread per one unit of recoil
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.96
+SWEP.ShootingSpeedMult = 1 // slow down applied while shooting
+SWEP.SightedSpeedMult = 0.5
+SWEP.MeleeSpeedMult = 0.5
+SWEP.MeleeSpeedMultTime = 2 // seconds to apply slow down for
+
+SWEP.SprintToFireTime = 0.35 // multiplies how long it takes to recover from sprinting
+
+// hold types
+
+SWEP.HoldType = "ar2"
+SWEP.HoldTypeSprint = "passive"
+SWEP.HoldTypeBlindFire = false
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(0, 0, 0)
+SWEP.PassivePos = Vector(0, -2, -4)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(0, -2, -4)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+SWEP.SightAng = Angle(0, -1, 0)
+SWEP.SightPos = Vector(-5.38, 0, -2.03)
+
+// reload
+
+SWEP.ClipSize = 30
+SWEP.Ammo = "smg1"
+
+SWEP.ReloadTimeMult = 1
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/m4.mdl"
+SWEP.DropMagazineImpact = "metal"
+
+// sounds
+
+local path = "tacint/weapons/m1/"
+
+SWEP.Sound_Shoot = "^" .. path .. "fire-1.wav"
+SWEP.Sound_Shoot_Silenced = path .. "fire_silenced-1.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+SWEP.EjectEffect = 2
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["foldstock"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+        BGs_WM = {
+            {1, 1}
+        }
+    },
+    ["tactical"] = {
+        BGs_VM = {
+            {2, 1}
+        },
+    },
+}
+
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "m1_root",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/silencer_on.wav",
+        DetachSound = "tacint/weapons/silencer_off.wav",
+        Pos_VM = Vector(5, 0.075, 33.5),
+        Pos_WM = Vector(0.1, 29, -0.1),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, -90, 0),
+    },
+    [2] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "m1_root",
+        WMBone = "Box01",
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+        InstalledElements = {"tactical"},
+        Pos_VM = Vector(4, 0.75, 24),
+        Pos_WM = Vector(0.9, 17, -1),
+        Ang_VM = Angle(90, 0, 90),
+        Ang_WM = Angle(-70, -90, 0),
+    },
+    [3] = {
+        PrintName = "Perk",
+        Category = {"perk", "perk_melee", "perk_shooting", "perk_reload"},
+        AttachSound = "tacint/weapons/flashlight_on.wav",
+        DetachSound = "tacint/weapons/flashlight_off.wav",
+    },
+}
---
+++ lua/weapons/tacrp_uzi.lua
@@ -0,0 +1,242 @@
+SWEP.Base = "tacrp_base"
+SWEP.Spawnable = true
+
+AddCSLuaFile()
+
+// names and stuff
+SWEP.PrintName = "IMI Mini Uzi"
+SWEP.Category = "Tactical RP (Arctic)"
+
+SWEP.Description = "Bullet hose machine pistol with a high rate of fire."
+
+SWEP.ViewModel = "models/weapons/tacint/v_uzi.mdl"
+SWEP.WorldModel = "models/weapons/tacint/w_uzi.mdl"
+
+SWEP.Slot = 2
+
+// "ballistics"
+
+SWEP.Damage_Max = 100 / 5 // damage at minimum range
+SWEP.Damage_Min = 100 / 8 // damage at maximum range
+SWEP.Range_Min = 1000 // distance for which to maintain maximum damage
+SWEP.Range_Max = 3000 // distance at which we drop to minimum damage
+SWEP.Penetration = 2 // units of metal this weapon can penetrate
+
+SWEP.MuzzleVelocity = 12500
+
+// misc. shooting
+
+SWEP.Firemodes = {
+    2,
+    1
+}
+
+SWEP.RPM = 950
+
+SWEP.Spread = 0.015
+
+SWEP.ShootSpeedMult = 0.5
+
+// Spread penalties are in spread units and are additive
+SWEP.MoveSpreadPenalty = 0.0 // spread penalty while travelling at max. 250 u/s
+SWEP.MidAirSpreadPenalty = 0.0 // spread penalty for being in the air
+SWEP.HipFireSpreadPenalty = 0.0 // spread penalty for not being scoped in
+SWEP.ScopedSpreadPenalty = 0
+SWEP.BlindFireSpreadPenalty = 0.0 // spread penalty for blind firing
+SWEP.CrouchSpreadPenalty = 0
+
+SWEP.RecoilPerShot = 1
+SWEP.RecoilMaximum = 10
+SWEP.RecoilResetTime = 0.1// time after you stop shooting for recoil to start dissipating
+SWEP.RecoilDissipationRate = 10
+SWEP.RecoilFirstShotMult = 1 // multiplier for the first shot's recoil amount
+
+SWEP.RecoilVisualKick = 1
+
+SWEP.RecoilKick = 1.5
+
+SWEP.RecoilSpreadPenalty = 0.0003 // extra spread per one unit of recoil
+
+SWEP.CanBlindFire = true
+
+// handling
+
+SWEP.MoveSpeedMult = 0.95
+SWEP.ShootingSpeedMult = 1 // slow down applied while shooting
+SWEP.SightedSpeedMult = 0.5
+SWEP.MeleeSpeedMult = 0.5
+SWEP.MeleeSpeedMultTime = 2 // seconds to apply slow down for
+
+SWEP.AimDownSightsTime = 0.275
+SWEP.SprintToFireTime = 0.30 // multiplies how long it takes to recover from sprinting
+
+// hold types
+
+SWEP.HoldType = "revolver"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeBlindFire = "pistol"
+
+SWEP.GestureShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.GestureReload = ACT_HL2MP_GESTURE_RELOAD_SMG1
+
+SWEP.PassiveAng = Angle(-2.5, -1, 0)
+SWEP.PassivePos = Vector(0, -2, -4)
+
+SWEP.BlindFireAng = Angle(0, 5, 0)
+SWEP.BlindFirePos = Vector(0, -2, -4)
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, 0, -2)
+
+SWEP.SightAng = Angle(0, 0, 1)
+SWEP.SightPos = Vector(-3.65, 0, -3.4)
+
+SWEP.Sway = 2
+SWEP.ScopedSway = 0.25
+
+// reload
+
+SWEP.ClipSize = 30
+SWEP.Ammo = "pistol"
+
+SWEP.ReloadTimeMult = 1
+SWEP.DropMagazineModel = "models/weapons/tacint/magazines/uzi.mdl"
+SWEP.DropMagazineImpact = "pistol"
+
+// sounds
+
+local path = "tacrp/weapons/uzi/"
+
+SWEP.Sound_Shoot = "^" .. path .. "fire-1.wav"
+SWEP.Sound_Shoot_Silenced = path .. "fire_silenced-1.wav"
+
+SWEP.Vol_Shoot = 110
+SWEP.ShootPitchVariance = 2.5 // amount to vary pitch by each shot
+
+// effects
+
+// the .qc attachment for the muzzle
+SWEP.QCA_Muzzle = 1
+// ditto for shell
+SWEP.QCA_Eject = 2
+
+SWEP.MuzzleEffect = "muzzleflash_ak47"
+
+// anims
+
+// VM:
+// idle
+// fire
+// fire1, fire2...
+// dryfire
+// melee
+// reload
+// midreload
+// prime_grenade
+// throw_grenade
+// throw_grenade_underhand
+// deploy
+// blind_idle
+// blind_fire
+// blind_fire1, blind_fire2...
+// blind_dryfire
+
+// WM:
+// attack1
+SWEP.AnimationTranslationTable = {
+    ["fire_iron"] = "dryfire",
+    ["fire1"] = "fire1_M",
+    ["fire2"] = "fire2_M",
+    ["fire3"] = "fire3_M",
+    ["fire4"] = "fire4_M",
+    ["fire5"] = "fire5_M",
+    ["melee"] = {"melee1", "melee2"}
+}
+
+// attachments
+
+SWEP.AttachmentElements = {
+    ["foldstock"] = {
+        BGs_VM = {
+            {1, 1}
+        },
+        BGs_WM = {
+            {1, 1}
+        }
+    },
+    ["tactical"] = {
+        BGs_VM = {
+            {3, 1}
+        },
+    },
+    ["smallmag"] = {
+        BGs_VM = {
+            {2, 1}
+        },
+        BGs_WM = {
+            {2, 1}
+        }
+    },
+}
+
+SWEP.Attachments = {
+    [1] = {
+        PrintName = "Optic",
+        Category = {"optic_cqb", "optic_medium"},
+        Bone = "ValveBiped.uzi_rootbone",
+        AttachSound = "TacRP/weapons/optic_on.wav",
+        DetachSound = "TacRP/weapons/optic_off.wav",
+        InstalledElements = {"optic"},
+        Pos_VM = Vector(-5, -0.25, 2.75),
+        Pos_WM = Vector(7, 1.5, -5.5),
+        Ang_VM = Angle(90, 0, 0),
+        Ang_WM = Angle(0, 0, 180),
+    },
+    [2] = {
+        PrintName = "Muzzle",
+        Category = "silencer",
+        Bone = "ValveBiped.uzi_rootbone",
+        AttachSound = "TacRP/weapons/silencer_on.wav",
+        DetachSound = "TacRP/weapons/silencer_off.wav",
+        VMScale = 1,
+        WMScale = 1,
+        Pos_VM = Vector(-3.15, -0.25, 13.5),
+        Ang_VM = Angle(90, 0, 180),
+        Pos_WM = Vector(18, 1.2, -3.8),
+        Ang_WM = Angle(0, 0, 180),
+    },
+
+    [3] = {
+        PrintName = "Tactical",
+        Category = "tactical",
+        Bone = "ValveBiped.uzi_rootbone",
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+        VMScale = 1,
+        WMScale = 1,
+        Pos_VM = Vector(-2, -0.25, 7.5),
+        Ang_VM = Angle(90, 0, 180),
+        Pos_WM = Vector(11, 1.2, -2.5),
+        Ang_WM = Angle(0, 0, 0),
+    },
+    [4] = {
+        PrintName = "Accessory",
+        Category = {"foldstock", "perk", "perk_melee", "perk_shooting", "perk_reload", "perk_hollowpoints"},
+        AttachSound = "TacRP/weapons/flashlight_on.wav",
+        DetachSound = "TacRP/weapons/flashlight_off.wav",
+    },
+}
+
+local function addsound(name, spath)
+    sound.Add({
+        name = name,
+        channel = 16,
+        volume = 1.0,
+        sound = spath
+    })
+end
+
+addsound("tacint_uzi.insert_clip", path .. "insert_clip.wav")
+addsound("tacint_uzi.remove_clip", path .. "remove_clip.wav")
+addsound("tacint_uzi.bolt_action", path .. "bolt_action.wav")
+addsound("tacint_uzi.foldingstock_back", path .. "foldingstock_back.wav")
