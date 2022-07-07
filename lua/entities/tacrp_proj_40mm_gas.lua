029922cf21798e449e5bc847238e7f0bef5b5433
TheOnly8Z <888888zombies@gmail.com>
2022-07-06 19:08:49 -0500

    hair trigger and 40mm gas

---
+++ lua/entities/tacrp_proj_40mm_gas.lua
@@ -0,0 +1,42 @@
+AddCSLuaFile()
+
+ENT.Base                     = "tacrp_proj_base"
+ENT.PrintName                = "40mm CS Gas"
+ENT.Spawnable                = false
+
+ENT.Model                    = "models/weapons/tacint/grenade_40mm.mdl"
+
+ENT.IsRocket = false // projectile has a booster and will not drop.
+
+ENT.InstantFuse = false // projectile is armed immediately after firing.
+ENT.RemoteFuse = false // allow this projectile to be triggered by remote detonator.
+ENT.ImpactFuse = true
+
+ENT.ExplodeOnDamage = false // projectile explodes when it takes damage.
+ENT.ExplodeUnderwater = true
+
+ENT.Delay = 0
+
+ENT.ExplodeSounds = {
+    "TacRP/weapons/grenade/smoke_explode-1.wav",
+}
+
+function ENT:Detonate()
+    if self:WaterLevel() > 0 then self:Remove() return end
+
+    util.BlastDamage(self, self:GetOwner(), self:GetPos(), 300, 25)
+
+    self:EmitSound(table.Random(self.ExplodeSounds), 75)
+
+    local cloud = ents.Create( "TacRP_gas_cloud" )
+
+    if !IsValid(cloud) then return end
+
+    cloud:SetPos(self:GetPos())
+    cloud:SetOwner(self:GetOwner())
+    cloud:Spawn()
+
+    self:Remove()
+end
+
+ENT.SmokeTrail = true
