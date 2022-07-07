990f95d8303dc98225c7088cb316e6d4b1edcc06
TheOnly8Z <888888zombies@gmail.com>
2022-07-07 14:32:11 -0500

    acc_aim

---
+++ lua/tacrp/shared/atts/acc_aim.lua
@@ -0,0 +1,10 @@
+ATT.PrintName = "Snapshot"
+ATT.Icon = Material("entities/tacrp_att_acc_aim.png", "mips smooth")
+ATT.Description = "Faster sight time."
+ATT.Pros = {"Aim down sights"}
+
+ATT.Category = "perk" // can be "string" or {"list", "of", "strings"}
+
+ATT.SortOrder = 2
+
+ATT.Mult_AimDownSightsTime = 0.85
