49623961ae3c79784d1df7c4364b973931486161
Arctic <mohaodong@gmail.com>
2022-07-08 13:49:32 +1000

    Limit equipment slot too

--- lua/tacrp/shared/sh_weaponlimit.lua
+++ lua/tacrp/shared/sh_weaponlimit.lua
@@ -1,7 +1,7 @@
 hook.Add( "PlayerCanPickupWeapon", "TacRP_Pickup", function( ply, wep )
     if GetConVar("tacrp_limitslots"):GetBool() then
         for k, v in pairs( ply:GetWeapons() ) do
-            if (wep:GetSlot() == 2 or wep:GetSlot() == 3) and (v:GetSlot() == wep:GetSlot()) then
+            if (wep:GetSlot() == 2 or wep:GetSlot() == 3 or wep:GetSlot() == 4) and (v:GetSlot() == wep:GetSlot()) then
                 return false
             end
         end
