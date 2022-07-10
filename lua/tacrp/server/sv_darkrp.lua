29414f890487192e75f44cdfb719d9c0bf66f2cd
TheOnly8Z <888888zombies@gmail.com>
2022-07-10 02:23:38 -0500

    It Just Works

--- lua/tacrp/server/sv_darkrp.lua
+++ lua/tacrp/server/sv_darkrp.lua
@@ -1,6 +1,7 @@
 util.AddNetworkString("tacrp_spawnedwepatts")
 
 hook.Add("onDarkRPWeaponDropped", "TacRP", function(ply, ent, wep)
+    ent:SetCollisionGroup(COLLISION_GROUP_WEAPON)
     if wep.ArcticTacRP and wep.Attachments then
         local atts = {}
         for k, v in pairs(wep.Attachments or {}) do
@@ -15,15 +16,18 @@ hook.Add("onDarkRPWeaponDropped", "TacRP", function(ply, ent, wep)
                     net.WriteUInt(v, TacRP.Attachments_Bits)
                 end
             net.Broadcast()
+            ent.Attachments = atts
         end
-        ent.Attachments = atts
     end
 end)
 
 hook.Add("PlayerPickupDarkRPWeapon", "TacRP", function(ply, ent, wep)
     if wep.ArcticTacRP and wep.Attachments and ent.Attachments then
         -- DarkRP will remove wep (created with ents.Create?) so we must make one ourselves here too
-        local has = ply:HasWeapon(wep:GetClass())
+        if ply:HasWeapon(wep:GetClass()) or ply:KeyDown(IN_WALK) then
+            ply:PickupObject(ent)
+            return true
+        end -- block duplicate pickups
 
         local class = wep:GetClass()
         wep:Remove()
@@ -37,14 +41,25 @@ hook.Add("PlayerPickupDarkRPWeapon", "TacRP", function(ply, ent, wep)
 
         ent.Attachments = nil -- Don't duplicate attachments
 
-        if has then
-            return true
-        end
-
         hook.Call("playerPickedUpWeapon", nil, ply, ent, wep)
-        ent:GivePlayerAmmo(ply, wep, has)
+        ent:GivePlayerAmmo(ply, wep, false)
         ent:DecreaseAmount()
 
         return true
     end
 end)
+
+local function hack()
+    local spawned_weapon = (scripted_ents.GetStored("spawned_weapon") or {}).t
+    if spawned_weapon and not spawned_weapon.TacRP_Hack then
+        spawned_weapon.TacRP_Hack = true -- don't make this change multiple times!
+        local old = spawned_weapon.StartTouch
+
+        function spawned_weapon:StartTouch(ent)
+            if ent.Attachments then return end
+            old(self, ent)
+        end
+    end
+end
+hook.Add("InitPostEntity", "TacRP_SpawnedWeaponHack", hack)
+hack()
