8bbe31e72a819ada596e3f8b7ffe50505a8dffb8
TheOnly8Z <888888zombies@gmail.com>
2022-07-25 23:57:48 -0500

    shuffle around lbar hbar stats

--- lua/tacrp/shared/atts/muzz_hbar.lua
+++ lua/tacrp/shared/atts/muzz_hbar.lua
@@ -1,18 +1,18 @@
 ATT.PrintName = "Heavy Barrel"
 ATT.Icon = Material("entities/tacrp_att_muzz_hbar.png", "mips smooth")
-ATT.Description = "Increases precision at the cost of sway."
-ATT.Pros = {"Spread", "Range"}
-ATT.Cons = {"Sway", "Recoil"}
+ATT.Description = "Improves sway, at the cost of range."
+ATT.Pros = {"Sway", "Recoil"}
+ATT.Cons = {"Spread", "Range"}
 
 ATT.Category = {"silencer", "barrel"} // can be "string" or {"list", "of", "strings"}
 
 ATT.SortOrder = 1
 
-ATT.Mult_Spread = 0.5
-ATT.Mult_RecoilSpreadPenalty = 0.8
-ATT.Mult_Range_Min = 0.75
-ATT.Mult_Range_Max = 1.15
-ATT.Mult_MuzzleVelocity = 1.25
-ATT.Mult_RecoilKick = 1.25
-ATT.Mult_Sway = 1.25
-ATT.Mult_ScopedSway = 1.25
+ATT.Mult_Spread = 1.25
+ATT.Mult_Range_Min = 1.5
+ATT.Mult_Range_Max = 0.75
+ATT.Mult_MuzzleVelocity = 0.8
+ATT.Mult_RecoilKick = 0.6
+ATT.Mult_Sway = 0.5
+ATT.Mult_ScopedSway = 0.5
+ATT.Mult_RecoilSpreadPenalty = 1.1
