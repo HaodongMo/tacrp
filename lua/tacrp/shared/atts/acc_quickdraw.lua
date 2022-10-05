4486056dd7760994da0346c675b281c092729f94
TheOnly8Z <888888zombies@gmail.com>
2022-10-04 22:54:04 -0500

    add quickdraw

---
+++ lua/tacrp/shared/atts/acc_quickdraw.lua
@@ -0,0 +1,10 @@
+ATT.PrintName = "Quickdraw"
+ATT.Icon = Material("entities/tacrp_att_acc_quickdraw.png", "mips smooth")
+ATT.Description = ""
+ATT.Pros = {"Faster draw speed"}
+
+ATT.Category = "acc_holster" // can be "string" or {"list", "of", "strings"}
+
+ATT.SortOrder = 4
+
+ATT.Mult_DeployTimeMult = 0.6
