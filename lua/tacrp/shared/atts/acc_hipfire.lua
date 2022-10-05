b23c46b4b0e84a1f6fdd92ef7457bc2c24448600
TheOnly8Z <888888zombies@gmail.com>
2022-10-04 21:14:54 -0500

    Rambo perk

---
+++ lua/tacrp/shared/atts/acc_hipfire.lua
@@ -0,0 +1,11 @@
+ATT.PrintName = "Rambo"
+ATT.Icon = Material("entities/tacrp_att_acc_hipfire.png", "mips smooth")
+ATT.Description = "Reduce hip spread."
+ATT.Pros = {"Hip fire spread", "Mid-air spread"}
+
+ATT.Category = "perk" // can be "string" or {"list", "of", "strings"}
+
+ATT.SortOrder = 2
+
+ATT.Mult_HipFireSpreadPenalty = 0.7
+ATT.Mult_MidAirSpreadPenalty = 0.5
