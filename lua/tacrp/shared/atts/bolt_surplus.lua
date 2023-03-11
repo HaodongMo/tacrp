2774010bcfe17c7a8bfbc922db60426741c5803f
Haodong Mo <mohaodong@gmail.com>
2023-03-11 22:40:45 +1000

    Surplus bolt

---
+++ lua/tacrp/shared/atts/bolt_surplus.lua
@@ -0,0 +1,12 @@
+ATT.PrintName = "Surplus"
+ATT.FullName = "Surplus Bolt"
+ATT.Icon = Material("entities/tacrp_att_bolt_surplus.png", "mips smooth")
+ATT.Description = "Rust has eaten most of it away, but it still kinda works."
+ATT.Cons = {"Unreliable"}
+
+ATT.Category = {"bolt_manual", "bolt_automatic"}
+
+ATT.Mult_MoveSpeedMult = 1.01
+ATT.Mult_AimDownSightsTime = 0.99
+
+ATT.Mult_ShootChance = 0.95
