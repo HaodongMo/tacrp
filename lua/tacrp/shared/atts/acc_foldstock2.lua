9d02575297483912541682a0255fff0f0a155a92
TheOnly8Z <888888zombies@gmail.com>
2023-03-12 01:50:22 -0600

    adjust stock uses unholster anim too

--- lua/tacrp/shared/atts/acc_foldstock2.lua
+++ lua/tacrp/shared/atts/acc_foldstock2.lua
@@ -1,7 +1,7 @@
 ATT.PrintName = "Adjust Stock"
 ATT.Icon = Material("entities/tacrp_att_acc_foldstock.png", "mips smooth")
 ATT.Description = "Shorten stock to improve handling somewhat at the cost of recoil."
-ATT.Pros = {"Handling"}
+ATT.Pros = {"Handling", "Draw Speed"}
 ATT.Cons = {"Recoil Kick", "Sway"}
 
 ATT.Free = true
@@ -10,15 +10,15 @@ ATT.Category = "acc_foldstock2"
 
 ATT.SortOrder = 0.5
 
-ATT.Mult_VisualRecoilKick = 1.5
-ATT.Mult_RecoilKick = 1.5
+ATT.Mult_VisualRecoilKick = 1.65
+ATT.Mult_RecoilKick = 1.65
 ATT.Mult_SprintToFireTime = 0.85
 ATT.Mult_AimDownSightsTime = 0.85
-ATT.Mult_SightedSpeedMult = 1.15
+ATT.Mult_SightedSpeedMult = 1.125
 
 ATT.Mult_ScopedSway = 1.5
 ATT.Mult_Sway = 1.25
 
 ATT.InstalledElements = {"foldstock"}
 
-ATT.TryUnholster = false
+ATT.TryUnholster = true
