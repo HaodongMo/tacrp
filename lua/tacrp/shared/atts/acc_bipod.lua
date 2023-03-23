e4f57f6d1fa298387efcecf28af16a552fa47555
TheOnly8Z <888888zombies@gmail.com>
2023-03-23 17:57:58 -0500

    bipod bg

--- lua/tacrp/shared/atts/acc_bipod.lua
+++ lua/tacrp/shared/atts/acc_bipod.lua
@@ -1,6 +1,6 @@
 ATT.PrintName = "Bipod"
 ATT.Icon = Material("entities/tacrp_att_acc_bipod.png", "mips smooth")
-ATT.Description = "Foldable support that stabilized the weapon when deployed."
+ATT.Description = "Foldable support that stabilizes the weapon when deployed."
 ATT.Pros = {"Crouching Recoil", "Crouching Sway"}
 
 ATT.Category = "acc_bipod"
@@ -9,3 +9,5 @@ ATT.SortOrder = 5.5
 
 ATT.Mult_RecoilCrouchMult = 0.5
 ATT.Mult_SwayCrouchMult = 0.25
+
+ATT.InstalledElements = {"bipod"}
