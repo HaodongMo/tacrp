864072153de778b9056015daeeb935941446d650
Sam Nyashev <12237569+Heyter@users.noreply.github.com>
2023-04-11 16:14:13 +0300

    Update sh_timers.lua

--- lua/weapons/tacrp_base/sh_timers.lua
+++ lua/weapons/tacrp_base/sh_timers.lua
@@ -1,12 +1,12 @@
 function SWEP:SetTimer(time, callback, id)
     if !IsFirstTimePredicted() then return end
 
-	self.ActiveTimers = self.ActiveTimers or {}
+    self.ActiveTimers = self.ActiveTimers or {}
     table.insert(self.ActiveTimers, { time + CurTime(), id or "", callback })
 end
 
 function SWEP:TimerExists(id)
-	if (!self.ActiveTimers) then return false end
+    if (!self.ActiveTimers) then return false end
 
     for _, v in ipairs(self.ActiveTimers) do
         if v[2] == id then return true end
@@ -24,28 +24,28 @@ function SWEP:KillTimer(id)
 end
 
 function SWEP:KillTimers()
-	if (!self.ActiveTimers) then return end
+    if (!self.ActiveTimers) then return end
 
-	-- memory
-	for k in ipairs(self.ActiveTimers) do
-		self.ActiveTimers[k] = nil
-	end
+    -- memory
+    for k in ipairs(self.ActiveTimers) do
+        self.ActiveTimers[k] = nil
+    end
 
     self.ActiveTimers = {}
 end
 
 function SWEP:ProcessTimers()
-	if (!self.ActiveTimers) then return end
+    if (!self.ActiveTimers) then return end
 
-	local timer = 0
+    local timer = 0
 
     for k, v in ipairs(self.ActiveTimers) do
-		timer = v[1]
+        timer = v[1]
 
-		if timer > UCT then
-			table.remove(self.ActiveTimers, k)
+        if timer > UCT then
+            table.remove(self.ActiveTimers, k)
         elseif timer <= UCT then
-			v[3]()
-		end
+            v[3]()
+        end
     end
 end
