4bb3662d23ab258d9529c5810e77861b1e1adaa7
TheOnly8Z <888888zombies@gmail.com>
2023-05-16 14:11:38 -0500

    steel core rounds for pistols

---
+++ lua/tacrp/shared/atts/ammo_pistol_ap.lua
@@ -0,0 +1,20 @@
+ATT.PrintName = "Steel Core"
+ATT.FullName = "Steel Core Rounds"
+
+ATT.Icon = Material("entities/tacrp_att_ammo_pistol_ap.png", "mips smooth")
+ATT.Description = "Hardened bullets penetrate much better but have less stopping power."
+ATT.Pros = {"Armor Penetration", "Material Penetration"}
+ATT.Cons = {"Damage", "Recoil Kick"}
+
+ATT.Category = "ammo_pistol"
+
+ATT.SortOrder = 1.5
+
+ATT.Mult_Damage_Max = 0.9
+ATT.Mult_Damage_Min = 0.9
+
+ATT.Add_Penetration = 5
+ATT.Add_ArmorPenetration = 0.15
+ATT.Add_ArmorBonus = 0.25
+
+ATT.Add_RecoilKick = 1
