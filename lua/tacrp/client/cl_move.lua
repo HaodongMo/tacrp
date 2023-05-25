1e26cdab78f88fb1336f9203b34618df1dce9904
TheOnly8Z <888888zombies@gmail.com>
2023-05-25 18:42:41 -0500

    autoreload considers infitnite ammo

--- lua/tacrp/client/cl_move.lua
+++ lua/tacrp/client/cl_move.lua
@@ -5,7 +5,7 @@ hook.Add("CreateMove", "TacRP_CreateMove", function(cmd)
     if !wpn.ArcticTacRP then return end
 
     if GetConVar("TacRP_autoreload"):GetBool() then
-        if wpn:Clip1() == 0 and wpn:Ammo1() > 0 and wpn:GetNextPrimaryFire() + 0.5 < CurTime() then
+        if wpn:Clip1() == 0 and (wpn:Ammo1() > 0 or wpn:GetInfiniteAmmo()) and wpn:GetNextPrimaryFire() + 0.5 < CurTime() then
             local buttons = cmd:GetButtons()
 
             buttons = buttons + IN_RELOAD
