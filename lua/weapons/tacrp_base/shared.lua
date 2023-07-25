6f6a2fcfef837586305c4c0d918b317ca9ba5694
TheOnly8Z <888888zombies@gmail.com>
2023-07-25 14:45:15 -0500

    oops

--- lua/weapons/tacrp_base/shared.lua
+++ lua/weapons/tacrp_base/shared.lua
@@ -238,7 +238,7 @@ SWEP.Melee2Delay = nil
 SWEP.MeleeThrowDamage = nil
 SWEP.MeleeThrowForce = 3000
 SWEP.MeleeThrowTime = nil // cooldown time, also controls animation speed
-SWEP.MeleeThrowTimeWait = nil // delay until deploy animation is played; does not affect actual cooldown
+SWEP.MeleeThrowTimeWait = 0.3 // delay until deploy animation is played; does not affect actual cooldown
 // used on some knife attachments
 SWEP.MeleeDashChargeTime = 7.5 // time taken to fully charge dash meter, lower is faster charge
 
