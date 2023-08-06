2cdc4ab182a08a41ed4c403c4bdb2d95825b7fd3
Fesiug <publicfesiug@outlook.com>
2023-08-06 08:56:41 -0400

    Document Match bloom modifiers

--- lua/tacrp/shared/atts/ammo_rifle_match.lua
+++ lua/tacrp/shared/atts/ammo_rifle_match.lua
@@ -2,7 +2,7 @@ ATT.PrintName = "Match"
 ATT.FullName = "Match Rounds"
 ATT.Icon = Material("entities/tacrp_att_acc_match.png", "mips smooth")
 ATT.Description = "Bullets with greatly improved accuracy."
-ATT.Pros = {"stat.spread", "stat.muzzlevelocity"}
+ATT.Pros = {"stat.spread", "stat.muzzlevelocity", "stat.recoilspread"}
 ATT.Cons = {"stat.hipfirespread", "att.procon.limb"}
 
 ATT.Category = "ammo_rifle"
@@ -10,9 +10,9 @@ ATT.Category = "ammo_rifle"
 ATT.SortOrder = 2
 
 ATT.Mult_Spread = 0.25
+ATT.Mult_MuzzleVelocity = 1.5
 ATT.Mult_RecoilSpreadPenalty = 0.85
 ATT.Add_HipFireSpreadPenalty = 0.015
-ATT.Mult_MuzzleVelocity = 1.5
 
 ATT.Override_BodyDamageMultipliersExtra = {
     [HITGROUP_LEFTARM] = 0.95,
