7bf2e23f3bf1e08ae80d1539b63c361215607fb6
TheOnly8Z <888888zombies@gmail.com>
2023-12-21 00:29:38 -0800

    fix instant melee on miss

--- lua/weapons/tacrp_base/sh_melee.lua
+++ lua/weapons/tacrp_base/sh_melee.lua
@@ -138,13 +138,15 @@ function SWEP:Melee(alt)
                 Src = start,
             })
         else
-            local tmiss = 0
+            local tmiss
             if !alt and self:GetValue("MeleeAttackMissTime") then
                 tmiss = self:GetValue("MeleeAttackMissTime")
             elseif alt then
                 tmiss = self:GetHeavyAttackTime(true, false)
             end
-            self:SetNextSecondaryFire(CurTime() + (tmiss - delay))
+            if tmiss then
+                self:SetNextSecondaryFire(CurTime() + (tmiss - delay))
+            end
             if delay == 0 then
                 self:EmitSound(self:ChooseSound(self:GetValue("Sound_MeleeSwing")), 75, 100, 1)
             end
