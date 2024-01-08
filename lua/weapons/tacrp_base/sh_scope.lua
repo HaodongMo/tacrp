19ed48b352506d650d4eec9a503ac84915fc3032
TheOnly8Z <888888zombies@gmail.com>
2024-01-08 03:29:45 -0800

    scopeless weapons can alt fire melee

--- lua/weapons/tacrp_base/sh_scope.lua
+++ lua/weapons/tacrp_base/sh_scope.lua
@@ -206,8 +206,7 @@ function SWEP:ThinkSights()
     local toggle = self:GetOwner():GetInfoNum("tacrp_toggleaim", 0) == 1
     local press, down = self:GetOwner():KeyPressed(IN_ATTACK2), self:GetOwner():KeyDown(IN_ATTACK2)
 
-    if self:DoOldSchoolScopeBehavior() and press then
-        self.Primary.Automatic = false
+    if (!self:GetValue("Scope") or self:DoOldSchoolScopeBehavior()) and down then
         self:Melee()
     elseif sighted and ((toggle and press) or (!toggle and !down)) then
         self:ScopeToggle(0)
