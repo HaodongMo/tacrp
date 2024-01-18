ff858f35bc48dee7fd296fb517b95422168c4545
TheOnly8Z <888888zombies@gmail.com>
2024-01-17 20:31:36 -0800

    side recoil spills into freeaim angle (if enabled)

--- lua/tacrp/shared/sh_move.lua
+++ lua/tacrp/shared/sh_move.lua
@@ -214,6 +214,13 @@ function TacRP.StartCommand(ply, cmd)
 
         recrise = TacRP.RecoilRise
 
+        if TacRP.ConVars["freeaim"]:GetBool() and wpn:GetValue("FreeAim") and wpn:GetScopeLevel() == 0 then
+            local freeaimang = wpn:GetFreeAimAngle()
+            siderec = siderec * 0.5
+            freeaimang:Add(Angle(0, siderec, 0))
+            wpn:SetFreeAimAngle(freeaimang)
+        end
+
         recrise = recrise + Angle(uprec, siderec, 0)
 
         TacRP.RecoilRise = recrise
