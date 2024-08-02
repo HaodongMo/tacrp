1525a9110703d3d1cb6e1bec8aedb89ec1f8e209
TheOnly8Z <888888zombies@gmail.com>
2024-08-03 00:07:34 +0800

    hook for modifying medkit heal amount

--- lua/weapons/tacrp_medkit.lua
+++ lua/weapons/tacrp_medkit.lua
@@ -203,6 +203,11 @@ function SWEP:Think()
         else
             local selfheal = self.HealTarget == self:GetOwner()
             local amt = TacRP.ConVars[selfheal and "medkit_heal_self" or "medkit_heal_others"]:GetInt()
+
+            local ret = hook.Run("TacRP_MedkitHeal", self, self:GetOwner(), self.HealTarget, {amt})
+            amt = ret and ret[1] or amt
+            if amt == 0 then return end
+
             self:SetClip1(self:Clip1() - 1)
             self.HealTarget:SetHealth(math.min(self.HealTarget:Health() + amt, self.HealTarget:GetMaxHealth()))
             self:SetNextPrimaryFire(CurTime() + TacRP.ConVars["medkit_interval"]:GetFloat())
