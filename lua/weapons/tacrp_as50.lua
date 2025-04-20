ade43ef40e4caca3d8789fcf1ffe1e3742faf7bb
TheOnly8Z <888888zombies@gmail.com>
2025-04-20 15:17:31 -0400

    rephrase as50 description to not overlap quote

--- lua/weapons/tacrp_as50.lua
+++ lua/weapons/tacrp_as50.lua
@@ -11,7 +11,7 @@ SWEP.Category = "Tactical RP"
 SWEP.SubCatTier = "1Elite"
 SWEP.SubCatType = "7Sniper Rifle"
 
-SWEP.Description = "Semi-automatic AMR that can easily decimate at any range. \nEquipped with a 12x scope by default. \nFar too heavy to swing, so bashing is out of the question."
+SWEP.Description = "Semi-automatic AMR that can easily decimate personnel and materiel at any distance. Too heavy to bash with.\nEquipped with a 12x scope by default."
 SWEP.Description_Quote = "As seen on Future Weapons."
 
 SWEP.Trivia_Caliber = ".50 BMG"
